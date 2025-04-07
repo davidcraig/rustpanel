6e4bfef8ecbb11adfc91959fcb18a3a3ffd223ec
davidcraig <davidcraig-uk@protonmail.com>
2025-04-07 14:29:47 +0100

    Initial working version (loads google)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use web_view::*;
+
 fn main() {
-    println!("Hello, world!");
+    let webview = web_view::builder()
+        .title("Desktop Webview")
+        .content(Content::Url("https://www.google.com"))
+        .size(800, 600)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .build()
+        .unwrap();
+
+    webview.run().unwrap();
 }
